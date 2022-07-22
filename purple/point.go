785f1c64c5e609bf780a514c0ad972559f716757
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-07-23 04:41:55 +1000

    MaxX and MaxY

--- purple/point.go
+++ purple/point.go
@@ -7,3 +7,19 @@ type Point struct {
 type BBox struct {
 	p1, p2 Point
 }
+
+func (b *BBox) MaxX() float64 {
+	if b.p1.x > b.p2.x {
+		return b.p1.x
+	} else {
+		return b.p2.x
+	}
+}
+
+func (b *BBox) MaxY() float64 {
+	if b.p1.y > b.p2.y {
+		return b.p1.y
+	} else {
+		return b.p2.y
+	}
+}
