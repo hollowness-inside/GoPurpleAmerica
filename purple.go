3ac7a56d0dd93358246e60ba21c6ca79a6efc0bf
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-07-23 00:18:46 +1000

    Struct for CL arguments

---
+++ purple.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type PurpleRaw struct {
+	counties       string
+	dataPath       string
+	year           string
+	colorTablePath string
+}
+
+func (p *PurpleRaw) Evaluate() (*Purple, error) {
+	purple := new(Purple)
+
+	if p.counties != "" {
+		counties := strings.Split(p.counties, ",")
+		purple.counties = counties
+	}
+
+	purple.dataPath = p.dataPath
+
+	if p.year != "" {
+		year, err := strconv.Atoi(p.year)
+		if err != nil {
+			return nil, err
+		}
+		purple.year = year
+	}
+
+	if p.colorTablePath != "" {
+		f, err := os.Open(p.colorTablePath)
+		if err != nil {
+			return nil, err
+		}
+
+		sc := bufio.NewScanner(f)
+
+		colors := [3][3]int{}
+		for i := 0; i < 3; i++ {
+			if !(sc.Scan() && sc.Err() == nil) {
+				return nil, sc.Err()
+			}
+
+			rgbText := sc.Text()
+			rgb := strings.Split(rgbText, " ")
+			r, err := strconv.Atoi(rgb[0])
+			if err != nil {
+				return nil, err
+			}
+
+			g, err := strconv.Atoi(rgb[1])
+			if err != nil {
+				return nil, err
+			}
+
+			b, err := strconv.Atoi(rgb[2])
+			if err != nil {
+				return nil, err
+			}
+
+			colors[i] = [3]int{r, g, b}
+		}
+
+		purple.colors = colors
+	}
+
+	return purple, nil
+}
+
+type Purple struct {
+	counties []string
+	dataPath string
+	year     int
+	colors   [3][3]int
+}
