038f2c04caca83b410c2a2e69c2ca1de83bfb862
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-07-22 17:08:57 +1000

    Simple mockup

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
 func main() {
+	// TODO: Map scale
+	// TODO: County abbreviation or full name
+	// TODO: Geometric data by county
+	// TODO: Geometric data by several counties
+	// TODO: Election results by county
+	// TODO: Election results by several counties
+	// TODO: Colors lookup table
+	// TODO: Use different data sets
 
+	// Draws outline of the county
+	// purple --county COUNTY
+	// purple -c COUNTY
+
+	// Draws purple map representing election results
+	// purple --county COUNTY --data elections.zip --year 2015
+	// purple -c COUNTY -d elections.zip -y 2015
+
+	// Creates colors.txt, lookup color table
+	// purple --new-color-table [colors.txt]
+	// purple -N [colors.txt]
+
+	// Uses the specified lookup color table
+	// purple --colors colors.txt
+	// purple -n colors.txt
 }
