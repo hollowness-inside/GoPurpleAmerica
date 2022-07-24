387274cc47b00c19fcc127afc4f18f499de1da90
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-07-25 02:59:38 +1000

    drawCounty implemented

--- purple/painter.go
+++ purple/painter.go
@@ -1,16 +1,13 @@
 package purple
 
 import (
-	"image"
 	"image/color"
-	"image/png"
-	"math"
-	"os"
+
+	"github.com/llgcode/draw2d/draw2dsvg"
 )
 
 type Painter struct {
 	purple *Purple
-	image  *image.RGBA
 }
 
 func NewPainter(purple *Purple) Painter {
@@ -21,89 +18,40 @@ func NewPainter(purple *Purple) Painter {
 
 func (p *Painter) Draw() {
 	counties, _ := p.purple.ParseCounties()
-	county := counties[0]
 
-	scale := float64(p.purple.scale)
+	svg := draw2dsvg.NewSvg()
+	gc := draw2dsvg.NewGraphicContext(svg)
 
-	x0 := county.Bbox.MinX() * scale
-	y0 := county.Bbox.MinY() * scale
+	for _, county := range counties {
+		p.drawCounty(county, gc)
+	}
 
-	x1 := county.Bbox.MaxX() * scale
-	y1 := county.Bbox.MaxY() * scale
+	draw2dsvg.SaveToSvgFile("myfile.svg", svg)
+}
 
-	width := int(x1 - x0)
-	height := int(y1 - y0)
+func (p *Painter) drawCounty(county *County, gc *draw2dsvg.GraphicContext) {
+	minX := county.Bbox.MinX()
+	maxY := county.Bbox.MaxY()
 
-	img := image.NewRGBA(image.Rect(0, 0, width+1, height+1))
-	p.image = img
+	// gc.SetFillColor(color.RGBA{255,0,0,255})
+	gc.SetStrokeColor(color.Black)
+	gc.SetLineWidth(0.5)
 
 	for _, subc := range county.Subcounties {
-		points := make([]Point, subc.PointsN)
-		for i, point := range subc.Points {
-			x := point.x*scale - x0
-			y := y1 - point.y*scale
-			// img.Set(int(x), int(y), color.White)
-			points[i] = Point{x, y}
-		}
-		p.DrawOutline(&points)
-	}
+		gc.BeginPath()
 
-	f, _ := os.Create("myfile.png")
-	defer f.Close()
+		start := subc.Points[0]
+		xs, ys := start.x-minX, maxY-start.y
+		gc.MoveTo(xs, ys)
 
-	png.Encode(f, img)
-}
-
-func (p *Painter) DrawOutline(pts *[]Point) {
-	points := *pts
-	prev := points[0]
-	for _, pt := range points[1:] {
-		p.DrawLine(pt, prev)
-		prev = pt
-	}
-
-	p.DrawLine(points[0], points[len(points)-1])
-}
-
-func (p *Painter) DrawLine(start, end Point) {
-	x0, y0 := start.x, start.y
-	x1, y1 := end.x, end.y
-
-	if x0 == x1 {
-		if y1 > y0 {
-			y0, y1 = y1, y0
+		for _, point := range subc.Points {
+			x := point.x - minX
+			y := maxY - point.y
+			gc.LineTo(x, y)
 		}
 
-		x := int(x0)
-		for y := y0; y <= y1; y += 0.1 {
-			p.image.Set(x, int(y), color.White)
-		}
-		return
-	}
-
-	if y0 == y1 {
-		if x1 > x0 {
-			x0, x1 = x1, x0
-		}
-
-		y := int(y0)
-		for x := x0; x <= x1; x += 0.1 {
-			p.image.Set(int(x), y, color.White)
-		}
-		return
-	}
-
-	dx := x1 - x0
-	dy := y1 - y0
-
-	distance := math.Sqrt(dx*dx + dy*dy)
-
-	angle := math.Atan2(dy, dx)
-
-	for i := 0.0; i < distance; i += 0.01 {
-		x := x0 + math.Cos(angle)*i
-		y := y0 + math.Sin(angle)*i
-
-		p.image.Set(int(x), int(y), color.White)
+		gc.LineTo(xs, ys)
+		gc.Close()
+		gc.FillStroke()
 	}
 }
