b0dbf8d9a50459e84b87b5b0812552297e026775
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-07-25 16:37:16 +1000

    Coloring fixed

--- purple/statistics.go
+++ purple/statistics.go
@@ -42,11 +42,11 @@ func ReadStatistics(r io.Reader) *Statistics {
 
 		sum := r1 + r2 + r3
 
-		r := uint8(r1 / sum)
-		g := uint8(r2 / sum)
-		b := uint8(r3 / sum)
+		r := uint8((r1 / sum) * 255)
+		g := uint8((r2 / sum) * 255)
+		b := uint8((r3 / sum) * 255)
 
-		stats.data[row[0]] = color.RGBA{255 * r, 255 * g, 255 * b, 255}
+		stats.data[row[0]] = color.RGBA{r, g, b, 255}
 	}
 
 	return stats
